ce3218ab01a9f22e9c85a433f96730c58a1a5e04
Sunset Wan <sunset.wan@assaabloy.com>
2022-12-06 00:28:45 +0800

    init

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "LottieBinary",
+    name: "",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LottieBinary",
-            targets: ["LottieBinary"]),
+            name: "Lottie",
+            targets: ["Lottie"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LottieBinary",
-            dependencies: []),
-        .testTarget(
-            name: "LottieBinaryTests",
-            dependencies: ["LottieBinary"]),
+        .binaryTarget(name: "Lottie",
+                      path: "./Sources/Lottie.xcframework")
     ]
 )
--- Sources/LottieBinary/LottieBinary.swift
+++
@@ -1,6 +0,0 @@
-public struct LottieBinary {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
